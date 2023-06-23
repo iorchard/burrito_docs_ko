da4797f21567e4527e03d9c0a060eb3327314e6e
Heechul Kim <jijisa@iorchard.net>
2023-06-23 18:07:12 +0900

    first commit

---
+++ source/conf.py
@@ -0,0 +1,13 @@
+project = 'Burrito Documentation (Korean)'
+copyright = '2023, Burrito authors and contributors'
+author = 'Burrito authors and contributors'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,12 @@
+Burrito 한글 문서
+=================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+설치가이드
+
+* 온라인
+* 오프라인
+
