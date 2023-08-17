66e98ee6773cf690a3667b1720ffbbc343708555
unknown <thehappyy7@gmail.com>
2023-08-17 18:37:13 +0900

    commit

---
+++ source/conf.py
@@ -0,0 +1,16 @@
+project = 'Burrito Documentation (Korean)'
+copyright = '2023, Burrito authors and contributors'
+author = 'Burrito authors and contributors'
+
+extensions = [
+  'sphinx_toolbox.collapse',
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
